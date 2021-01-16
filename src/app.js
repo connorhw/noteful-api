4cc5fb88929c4dd2f2d0ad5e83840688b3cc87dd
connorhw <connorwong93@gmail.com>
2021-01-15 16:04:28 -0800

    went back to Deploy a server seection and added configs

--- src/app.js
+++ src/app.js
@@ -9,11 +9,9 @@ const notesRouter = require('./notes/notes-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan(morganOption))
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 app.use('/api/folders', foldersRouter)
--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const db = knex({
 
 app.set('db', db)
 
+const PORT = process.env.PORT || 8000
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
